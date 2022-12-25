f85492cdd8b33cc5c9eb64e7c4c1bb4149b72186
Yuri <57116431+softicer-67@users.noreply.github.com>
2022-12-25 07:52:59 +0500

    Add files via upload

---
+++ main_a.py
@@ -0,0 +1,33 @@
+from itertools import permutations
+
+
+def func(alphabet, min_len, max_len):
+    joiner = ''.join
+    for idx in range(min_len, max_len + 1):
+        yield from map(joiner, permutations(alphabet, idx))
+
+
+txt = 'gfg'
+result = []
+txt = list(txt)
+
+d = {
+    'Шесть': 6,
+    'Семь': 7,
+    'Восемь': 8,
+    'Девять': 9,
+    'Десять': 10,
+}
+
+res = func(txt, 0, 3)
+res = set(res)
+
+length = ''
+
+for k, v in d.items():
+    if v == len(set(res)):
+        length += k
+
+print(f'Строка: {txt}\n'
+      f'Результат: {len(set(res))}\n'
+      f'{length} различных подстрок: {set(res)}')
